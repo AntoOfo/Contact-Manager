b1cb212475732b7bca5e863ef7e44af6b04b8afa
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2024-08-10 21:08:46 +0100

    first input sorted, lots of comments for learning purposes

--- Contact_Manager.py
+++ Contact_Manager.py
@@ -5,33 +5,34 @@ import os
 filename = "contacts.csv"
 columns = ["first_name", "last_name", "phone", "email", "address"]
 
-df = pd.DataFrame(columns=columns)
+df = pd.DataFrame(columns=columns)   
     
-    if write_header:    # writes header only when neeeded
-        writer.writeheader()
-    
-    
-    while True:
-        user_input = int(input("1. Add new contacts \n2. View existing contacts \n3. Update contact details \n4. Delete contacts"))
-    
-        if user_input == 1:
-            pass
+while True:
+    user_input = int(input("1. Add new contacts \n2. View existing contacts \n3. Update contact details \n4. Delete contacts"))
+       
+    if user_input == 1:
+            user_data = {col: input(f"Enter {col}: ") for col in columns} # user_data = {first_name: "John"}
+            new_entry = pd.DataFrame([user_data], columns=columns)   # converts user data to df
+            df = pd.concat([df, new_entry], ignore_index=True)  # adds new entry to df
+            df.to_csv(filename, index=False)    # save df to file
             
-        elif user_input == 2:
-            pass
-        elif user_input == 3:
+            print("Contact added successfully.")
             
-            pass
+    elif user_input == 2:
+        pass
+    elif user_input == 3:
+            
+        pass
                 
-        elif user_input == 4:
-            pass
+    elif user_input == 4:
+        pass
             
 
 
-        continue_input = input("Do you want to continue? Y/N").strip().lower()
-        if continue_input == "N" or continue_input == "n":
-            print("Thanks for your time.")
-            break
+    continue_input = input("Do you want to continue? Y/N").strip().lower()
+    if continue_input == "N" or continue_input == "n":
+        print("Thanks for your time.")
+        break
         
     
     
